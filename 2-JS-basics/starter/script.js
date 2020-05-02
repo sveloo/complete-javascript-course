213176e42ac76e1485e672e0ae40b375faab098e
sveloo <sanny@edu360.com.au>
2020-05-02 19:09:36 +1000

    Completed Coding Challenge 6 - Dice Game

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -137,33 +137,152 @@ Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and heig
 GOOD LUCK 😀
 */
 
-var mark = {
-  fullName: "Mark Wahlberg",
-  mass: 200,
-  height: 1.7,
-  calcBmi: function (mass, height) {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
+// var mark = {
+//   fullName: "Mark Wahlberg",
+//   mass: 200,
+//   height: 1.7,
+//   calcBmi: function (mass, height) {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+
+// var john = {
+//   fullName: "Kevin Hart",
+//   mass: 200,
+//   height: 1.7,
+//   calcBmi: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+
+// if (mark.calcBmi() > john.calcBmi()) {
+//   console.log(mark.bmi, john.bmi);
+//   console.log("Mark's BMI is higher than John's");
+// } else if (john.calcBmi() > mark.calcBmi()) {
+//   console.log(mark.bmi, john.bmi);
+//   console.log("Mark's BMI is higher than John's");
+// } else {
+//   console.log(mark.bmi, john.bmi);
+//   console.log("Mark's BMI is equal to John's");
+// }
+
+/*****************************
+ * CODING CHALLENGE 5
+ */
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
 
 var john = {
-  fullName: "Kevin Hart",
-  mass: 200,
-  height: 1.7,
-  calcBmi: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
+  fullName: "John Smith",
+  bills: [124, 48, 268, 42],
+  tips: [],
+  totals: [],
+  averageTip: 0,
+  calcTip: function () {
+    for (var i = 0; i < this.bills.length; i++) {
+      var percentage, tip, total;
+      var bill = this.bills[i];
+
+      if (bill < 100) {
+        percentage = 0.2;
+      } else if (bill > 100 && bill < 300) {
+        percentage = 0.15;
+      } else percentage = 0.25;
+
+      tip = this.bills[i] * percentage;
+      total = this.bills[i] + tip;
+
+      this.tips.push(tip);
+      this.totals.push(total);
+    }
+  },
+  calcAverage: function () {
+    var sum = 0;
+    for (var i = 0; i < this.tips.length; i++) {
+      sum = sum + this.tips[i];
+    }
+    average = sum / this.tips.length;
+    this.averageTip = average;
   },
 };
 
-if (mark.calcBmi() > john.calcBmi()) {
-  console.log(mark.bmi, john.bmi);
-  console.log("Mark's BMI is higher than John's");
-} else if (john.calcBmi() > mark.calcBmi()) {
-  console.log(mark.bmi, john.bmi);
-  console.log("Mark's BMI is higher than John's");
-} else {
-  console.log(mark.bmi, john.bmi);
-  console.log("Mark's BMI is equal to John's");
-}
+var mark = {
+  fullName: "Mark Korn",
+  bills: [77, 375, 110, 45],
+  tips: [],
+  totals: [],
+  averageTip: 0,
+  calcTip: function () {
+    for (var i = 0; i < this.bills.length; i++) {
+      var percentage, tip, total;
+      var bill = this.bills[i];
+
+      if (bill < 100) {
+        percentage = 0.2;
+      } else if (bill > 100 && bill < 300) {
+        percentage = 0.15;
+      } else percentage = 0.1;
+
+      tip = this.bills[i] * percentage;
+      total = this.bills[i] + tip;
+
+      this.tips.push(tip);
+      this.totals.push(total);
+    }
+  },
+  calcAverage: function () {
+    var sum = 0;
+    for (var i = 0; i < this.tips.length; i++) {
+      sum = sum + this.tips[i];
+    }
+    average = sum / this.tips.length;
+    this.averageTip = average;
+  },
+};
+
+john.calcTip();
+john.calcAverage();
+mark.calcTip();
+mark.calcAverage();
+
+console.log(john.averageTip);
+
+if (john.averageTip > mark.averageTip) {
+  console.log(
+    "John's average tip of " +
+      john.averageTip +
+      " was more than Mark's average of " +
+      mark.averageTip
+  );
+} else if (mark.averageTip > john.averageTip) {
+  console.log(
+    "Mark's average tip of " +
+      mark.averageTip +
+      " was more than John's average of " +
+      john.averageTip
+  );
+} else console.log("Both have the same tipping average");
--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,25 +1,14 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+function calcAge(year) {
+  console.log(2020 - year);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+calcAge(1976);
 
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -37,8 +26,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +49,5 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
-
-
-
-
-
-
-
-
-
